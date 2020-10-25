6f3913baf7b9b90b88300c5fc594e96938246338
DSkoufis <d.skoufis@elsevier.com>
2020-10-25 22:11:46 +0000

    Moved to SpringBoot (tests)

---
+++ src/main/java/com/beretwood/cms/BeretWoodApplication.java
@@ -0,0 +1,13 @@
+package com.beretwood.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeretWoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeretWoodApplication.class, args);
+	}
+
+}
